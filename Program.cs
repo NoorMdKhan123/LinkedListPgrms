59ff6c2fd540f056a3730243d8d66738297d9c7a
NoorMdKhan123 <noormd6400@gmail.com>
2021-10-06 14:38:26 +0530

    creating simple linked list

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LinkedListAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedListClass list = new LinkedListClass();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+
+        }
+    }
+}
