165e4d63a6260dc1375c23a00552481ecea3e243
NoorMdKhan123 <noormd6400@gmail.com>
2021-10-07 14:24:08 +0530

    Done with adding numbers

---
+++ Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListAsign
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int i)
+        {
+            data = i;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LinkedListAsign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedListClass list = new LinkedListClass();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+
+        }
+    }
+}
